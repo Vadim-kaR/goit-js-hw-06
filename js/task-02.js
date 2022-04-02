5fc827c2405ae2085acb4472a066ca72e3081b29
unknown <raylyan.vadim@gmail.com>
2022-04-02 13:26:32 +0300

    task 2 done

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,17 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const list = document.querySelector("#ingredients")
+
+const result = ingredients.map(item => {
+  const itemCreate = document.createElement('li');
+  itemCreate.textContent = item;
+  itemCreate.classList.add("item");
+  return itemCreate;
+});
+
+
+list.append(...result);
+
+console.log(list);
