f006d10a4b64eb488bfcc10901d7e1dedb1738f9
unknown <raylyan.vadim@gmail.com>
2022-04-06 00:45:26 +0300

    task 5 done

---
+++ js/task-05.js
@@ -0,0 +1,15 @@
+const inputEl = document.querySelector('#name-input');
+const spanEl = document.querySelector('#name-output');
+
+inputEl.addEventListener('input', onInput)
+
+function onInput(event) { 
+
+    if (event.currentTarget.value === '') {
+        spanEl.textContent = 'Anonymous'
+    }
+
+    else {spanEl.textContent = event.currentTarget.value }
+
+}
+
