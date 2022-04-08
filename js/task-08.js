02fb7481541e371a549356e5e8622101f9b0eee8
unknown <raylyan.vadim@gmail.com>
2022-04-08 16:21:22 +0300

    task 9 version 1

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,31 @@
+const form = document.querySelector('.login-form');
+
+
+form.addEventListener('submit', onFormSubmit);
+
+function onFormSubmit(event) { 
+    event.preventDefault();
+  
+    const email = form.querySelector('[name="email"]')
+    const password = form.querySelector('[name="password"]')
+    
+    if (password.value === '' || email.value === '') { 
+       return alert('Пожалуйста, заполните все поля')
+    }
+
+    const object = {
+        email: email.value,
+        password: password.value,
+    }
+
+    event.currentTarget.reset();
+    
+    return  console.log(object);
+
+}
+
+function checkInputValue() { 
+
+}
+
 
