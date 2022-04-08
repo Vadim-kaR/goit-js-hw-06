a0652684321dfe5c83bc0f695d4206b353208c36
unknown <raylyan.vadim@gmail.com>
2022-04-08 19:12:17 +0300

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,16 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const bodySelector = document.querySelector('body');
+const buttonEl = document.querySelector(".change-color")
+const spanEl = document.querySelector('.color')
+
+buttonEl.addEventListener('click', changeBodyColor)
+
+function changeBodyColor() { 
+
+  bodySelector.style.backgroundColor = getRandomHexColor();
+  spanEl.textContent = getRandomHexColor();
+
+}
