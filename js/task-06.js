6ce7dd69df2f7c8ddbb0605b4586f8965ce8df17
unknown <raylyan.vadim@gmail.com>
2022-04-12 16:23:53 +0300

    task 6 fixed

--- js/task-06.js
+++ js/task-06.js
@@ -1,25 +1,20 @@
 const inputEl = document.querySelector("#validation-input")
-
-
 inputEl.addEventListener("blur", checkInputLength)
 
 function checkInputLength(event) {
-    
     const minInputLength = inputEl.dataset.length;
-    const currentInputValue = (event.currentTarget.value).length;
+    const currentInputValue = (event.currentTarget.value).length ;
 
-
-
-    if (minInputLength >= currentInputValue) {
-        invalidInputLength();
+     if (minInputLength <= currentInputValue) {
+            
+        validInputLength();
     }
     else {
-        validInputLength();
+        invalidInputLength();
     }
 }
 
 function validInputLength () { 
-
     inputEl.classList.replace('invalid', 'valid')
 }
 
